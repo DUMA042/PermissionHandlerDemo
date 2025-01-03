f96678e644aa02cfdac08071e8ac7bd086afa451
DUMA042 <dumebiikeoffiah@gmail.com>
2025-01-03 21:11:23 +0100

    For the Native permission

--- app/src/main/java/com/example/permissionhandlerdemo/MainActivity.kt
+++ app/src/main/java/com/example/permissionhandlerdemo/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
+        NotificationUtils.createNotificationChannel(this)
         setContent {
             PermissionHandlerDemoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
