66c61ac05d26c22cfb687554bb978b09040e44f4
DUMA042 <dumebiikeoffiah@gmail.com>
2025-01-03 21:11:58 +0100

    For the Native permission

---
+++ app/src/main/java/com/example/permissionhandlerdemo/HomeScreen.kt
@@ -0,0 +1,75 @@
+package com.example.permissionhandlerdemo
+
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.ui.Modifier
+import android.Manifest
+
+
+
+
+@Composable
+fun HomeScreen(permissionViewmodel: PermissionViewmodel=viewModel(),modifier: Modifier=Modifier){
+
+    val context = LocalContext.current
+
+
+    val toShowrational by  permissionViewmodel.showRationaleDialog
+    val permissionState by permissionViewmodel.permistionState
+
+    val permissionLauncher= rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission(),
+        onResult = { isGranted ->
+            if (isGranted) {
+                permissionViewmodel.updatePermistionState(true)
+            } else {
+
+                if (PermissionUtils.shouldShowRationale(context,  Manifest.permission.POST_NOTIFICATIONS)) {
+                    //will be changed
+                    permissionViewmodel.updateShowRational(true)
+                }
+                Toast.makeText(
+                    context,
+                    "Camera permission was denied.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    )
+
+
+
+    LaunchedEffect(Unit) {
+
+        permissionLauncher.launch( Manifest.permission.POST_NOTIFICATIONS)
+
+    }
+
+
+
+
+    if(toShowrational){
+        ShowRationaleDialog(
+            onDismiss = { permissionViewmodel.updateShowRational(false) },
+            onConfirm = {
+                permissionViewmodel.updateShowRational(false) // Dismiss the dialog
+                permissionLauncher.launch( Manifest.permission.POST_NOTIFICATIONS) // Retry permission
+            }
+        )
+
+    }
+
+    if(permissionState){
+        NotificationUtils.sendtheNotification(context,"Notification Demo","The permission has been accepted!!!")
+        Toast.makeText(context, "Permission Granted", Toast.LENGTH_SHORT).show()
+
+    }
+
+
+}
